2086915c959513772ea05b3f2105203c05fd1c76
Vinderblad7 <patkovskijtimofej@gmail.com>
2026-06-29 20:55:53 +0300

    add tags model and generate alembic migration

--- backend/src/posts/models.py
+++ backend/src/posts/models.py
@@ -1,7 +1,27 @@
 from src.database import Base
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Text, DateTime, func
-import datetime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Column, Integer, Text, DateTime, func, Table, ForeignKey
+import datetime 
+
+post_tags_table = Table(
+    "post_tags",
+    Base.metadata,
+    Column("post_id", Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True),
+    Column("tag_id", Integer, ForeignKey("tags.id", ondelete="CASCADE"), primary_key=True)
+)
+
+
+class TagsModel(Base):
+    __tablename__ = "tags"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    posts: Mapped[list["PostModel"]] = relationship(
+        secondary=post_tags_table,
+        back_populates="tags"      
+    )
+
 
 class PostModel(Base):
     __tablename__ = "posts"
@@ -11,3 +31,8 @@ class PostModel(Base):
     content: Mapped[str] = mapped_column(Text)
     created_at: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
     updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
+
+    tags: Mapped[list["TagsModel"]] = relationship(
+        secondary=post_tags_table, 
+        back_populates="posts"  
+    )
