61499dbf9ea55a949e034eaf1e65089a039fae21
Vinderblad7 <patkovskijtimofej@gmail.com>
2026-07-05 22:35:33 +0300

    add created_at and updated_at to CommentModel

--- backend/src/comments/models.py
+++ backend/src/comments/models.py
@@ -11,6 +11,8 @@ class CommentModel(Base):
 
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     post_id: Mapped[int] = mapped_column(ForeignKey("posts.id"))
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
 
     post: Mapped["PostModel"] = relationship(
     back_populates="comments",
